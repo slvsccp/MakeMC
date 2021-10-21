452118a6ae0b171b40efcd3c84516e1505abef07
Matheus silva <teus_26@hotmail.com>
2021-10-21 15:10:13 -0300

    aba Montar Burger

---
+++ src/views/MontarBurger.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-container">
+    <h1>Monte o seu Burger:</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+  import BurgerForm from '../components/BurgerForm.vue'
+
+  export default {
+    name: 'MontarBurger',
+    components: {
+      BurgerForm
+    }
+  }
+</script>
