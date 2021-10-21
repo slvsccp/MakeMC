3000ce0ba601429df13a201b73817e43f809b2f1
Matheus silva <teus_26@hotmail.com>
2021-10-21 15:08:31 -0300

    img Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main-banner">
+    <!---<h1>Make Your Burger</h1>--->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<style scoped>
+  #main-banner {
+    background-image: url('/img/burger-banner.png');
+    background-position: auto;
+    background-size: cover;
+    height: 550px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+</style>
