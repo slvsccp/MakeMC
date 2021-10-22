66ca9e613ba5adfddf2ebf5d2811375e9b7ab989
Matheus silva <teus_26@hotmail.com>
2021-10-22 13:50:33 -0300

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,20 @@
   <div>
     <Banner />
   </div>
+  <div>
+    <Main />
+  </div>
 </template>
 
 <script>
   import Banner from '../components/Banner.vue'
+  import Main from '../components/Main.vue'
 
   export default {
     name: 'Home',
     components: {
-      Banner
+      Banner,
+      Main
     }
   }
 </script>
