0df5b359b714422d8f931e3c1558e1366438b2e5
Matheus silva <teus_26@hotmail.com>
2021-10-22 13:51:22 -0300

    aba Meus pedidos

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="home-container">
-    <h1>PEDIDOS</h1>
+  <div class="main-container">
+    <h1>Meus pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
+
+<script>
+  import Dashboard from '../components/Dashboard.vue'
+
+export default {
+  name: 'Pedidos',
+  components: {
+    Dashboard
+  }
+}
+</script>
