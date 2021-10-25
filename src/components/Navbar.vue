e0ce8dadbd5c75b76d730daa0c16d23137389dd7
Matheus silva <teus_26@hotmail.com>
2021-10-25 17:53:12 -0300

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,15 +5,38 @@
     </router-link>
     <router-link to="/">Home</router-link> |
     <router-link to="/montarburger">Monte seu MakeBurger</router-link> |
-    <router-link to="/pedidos">Pedidos</router-link><!--- |
-    <router-link to="/trabalheConosco">Trabalhe Conosco</router-link>--->
+    <router-link to="/pedidos">Pedidos</router-link> |
+    <router-link to="/trabalheConosco">Trabalhe Conosco</router-link>
+
+    <!---<div id="multilinguas" :v-model="lang" @click="handleChange($event)">
+      <button type="button" class="btn btn-flag" value="pt-BR" style='display:flex;align-items:center;justify-content:center;'><img src="/img/Brazil.png" style="width:auto;height:25px;display:block;"></button>
+      <button type="button" class="btn btn-flag" value="en" style='display:flex;align-items:center;justify-content:center;'><img src="/img/Eua.png" style="width:auto;height:25px;display:block;"></button>
+    </div>--->
+    <!---<select :v-model="lang" @change="handleChange($event)">
+      <option value="" selected>Selecione</option>
+      <option value="en">English</option>
+      <option value="pt-BR">Brazil</option>
+      <option value="es">Spanish</option>
+    </select>--->
   </div>
 </template>
 
 <script>
 export default {
   name: "Navbar",
-  props: ["logo", "alt"]
+  props: ["logo", "alt"],
+  data: function() {
+    const lang = localStorage.getItem('lang') || 'en';
+    return {
+      lang: lang
+    }
+  },
+  methods: {
+    handleChange(event) {
+      localStorage.setItem('lang', event.target.value);
+      window.location.reload();
+    }
+  }
 }
 </script>
 
